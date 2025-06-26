66f3a30d737c70f0b33222a74b0278b55eb3494d
Jesus Vasquez <adonay16.vz@gmail.com>
2025-06-25 22:31:18 -0600

    feat: auth0 implementation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+/* import React from "react";
 import Header from "@/components/layout/Header";
 import Hero from "@/components/home/MainFold";
 
@@ -11,5 +11,40 @@ export default function HomePage() {
         <Hero />
       </main>
     </div>
+  );
+}
+ */
+
+import { auth0 } from "@/lib/auth0";
+import "./globals.css";
+
+export default async function Home() {
+  // Fetch the user session
+  const session = await auth0.getSession();
+
+  // If no session, show sign-up and login buttons
+  if (!session) {
+    return (
+      <main>
+        <a href="/auth/login?screen_hint=signup">
+          <button>Sign up</button>
+        </a>
+        <a href="/auth/login">
+          <button>Log in</button>
+        </a>
+      </main>
+    );
+  }
+
+  // If session exists, show a welcome message and logout button
+  return (
+    <main>
+      <h1>Welcome, {session.user.name}!</h1>
+      <p>
+        <a href="/auth/logout">
+          <button>Log out</button>
+        </a>
+      </p>
+    </main>
   );
 }
